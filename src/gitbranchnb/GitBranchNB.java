483b3e8308da5fd5a5ed3945c2f63c6728de395c
vikierdos <csicsorka@gmail.com>
2024-09-30 08:52:21 +0200

    statikus függvény létrejött

--- src/gitbranchnb/GitBranchNB.java
+++ src/gitbranchnb/GitBranchNB.java
@@ -3,7 +3,11 @@ package gitbranchnb;
 public class GitBranchNB {
 
     public static void main(String[] args) {
-        
+        fv();
+    }
+
+    private static void fv() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
     
 }
